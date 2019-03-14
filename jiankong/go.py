b3b049a195eed55c731f996808b2ae0971a64d16
woshichenya <bj_xiaoya@163.com>
2019-03-14 18:30:49 +0800

    1.0.0

---
+++ jiankong/go.py
@@ -0,0 +1,23 @@
+import login_china
+import time
+go=login_china.go
+
+t_name=[]
+name_qun=['系统管理','小程序管理','商品管理','订单管理','用户管理','分销中心','内容管理','营销管理','应用专区','操作员管理','系统工具']
+for name_qun_go in name_qun:
+    go.Ctext(name_qun_go, name_qun_go + "菜单", "进入" + name_qun_go + "菜单", "无法进入" + name_qun_go + "菜单")
+    ss=go.llq.find_elements_by_tag_name("span")
+    for sss in ss:
+        name_n = sss.text
+        if name_n !="":
+            t_name.append(name_n)
+    for name in t_name:
+        if name != "":
+            if name not in name_qun :
+                go.Ctext(name,name+"菜单","进入"+name+"菜单","无法进入"+name+"菜单")
+                time.sleep(2)
+    t_name=[]
+
+
+
+
